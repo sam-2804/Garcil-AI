722eb5c0b7790ed6ac4b616c0e1d5f7d67e69fd9
Sam <samdavid9xxx@gmail.com>
2026-09-18 13:22:11 +0530

    Version 0.1
    -First commit
    -Initialized the web app through streamlit

---
+++ app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Garcil-AI : Coming soon")
+st.header("Garcil-AI: An agentic RAG system that calculates skill gaps and dynamically generates stateful, interactive career roadmaps.")
